0afad54c333a8a907a6e192dcaf54fea51b474cf
Thilina Ruwan <106557565+thilinaruwan112@users.noreply.github.com>
2024-03-18 01:31:20 +0530

    add profile image

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -49,6 +49,31 @@ const Hero = () => {
 
           {/* Images */}
           <div className="hidden xl:flex relative">
+            {/* badge 1 */}
+            <Badge
+              containerStyles="absolute top-[24%] -left-[5rem]"
+              icon={<RiBriefcase4Fill />}
+              endCountNum={3}
+              badgeText="Years of Experience"
+            />
+
+            {/* Badge 2 */}
+            <Badge
+              containerStyles="absolute top-[80%] -left-[1rem]"
+              icon={<RiBriefcase4Fill />}
+              endCountNum={3}
+              endCountText="k"
+              badgeText="Finished Projects"
+            />
+
+            {/* Badge 3 */}
+            <Badge
+              containerStyles="absolute top-[55%] -right-8"
+              icon={<RiTeamFill />}
+              endCountNum={25}
+              endCountText="k"
+              badgeText="Happy Clients"
+            />
             <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
             <DevImg
               containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
