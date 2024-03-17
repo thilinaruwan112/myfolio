5c2edd27dcf6d2829096949bc7bae8b5f2239d39
Thilina Ruwan <106557565+thilinaruwan112@users.noreply.github.com>
2024-03-17 16:31:45 +0530

    Initial Stage

---
+++ components/Header.jsx
@@ -0,0 +1,33 @@
+"use client";
+import Logo from "./Logo";
+import MobileNav from "./MobileNav";
+import Nav from "./Nav";
+import ThemeToggler from "./ThemeToggler";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div className="flex items-center gap-x-6">
+            {/* Nav Bar */}
+            <Nav
+              containerStyles="hidden xl:flex gap-x-8 items-center"
+              linkStyles="relative hover:text-primary transition-all"
+              underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"
+            />
+            <ThemeToggler />
+
+            {/* Mobile Nav */}
+            <div className="xl:hidden">
+              <MobileNav />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/Logo.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
+import { HomeIcon } from "@radix-ui/react-icons";
+
+const Logo = () => {
+  return (
+    <Link className="" href="/">
+      <HomeIcon />
+    </Link>
+  );
+};
+
+export default Logo;
